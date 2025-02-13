b0b65ec3450562afd94db479053944b3e06e4984
KursedKourtKardash <khozakourtney@gmail.com>
2025-02-13 17:27:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Floating Letters Interaction
+document.querySelectorAll('.letter').forEach(letter => {
+  letter.addEventListener('click', () => {
+    alert(letter.innerText); // Displays the message when a letter is clicked
+  });
+});
+
+// Background Music Control (Optional)
+const bgMusic = document.getElementById('bg-music');
+bgMusic.play();
 
